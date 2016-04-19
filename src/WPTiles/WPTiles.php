05918d5e51574f7de00ec970c39d7ab3a49066f1
Julien Carignan <julien.carignan@atelierfolklore.ca>
2016-04-19 01:22:17 -0400

    more

--- src/WPTiles/WPTiles.php
+++ src/WPTiles/WPTiles.php
@@ -413,6 +413,7 @@ class WPTiles extends Abstracts\WPSingleton
                     $linkType = get_field('link-type', $postID);
                     $linkPostId = get_field('link-post-id', $postID);
                     $linkUrl = get_field('link-url', $postID);
+                    $linkTarget = get_field('target-type', $postID) ? '_blank':'_self';
                     $link;
                     if ($linkType === 'none')
                     {
@@ -437,16 +438,16 @@ class WPTiles extends Abstracts\WPSingleton
                 <div class='<?php echo implode( ' ', $tile_classes ) ?>' id='tile-<?php echo $post->ID ?>'>
                 <?php if ( 'post' == $opts['link'] && $link !== null) : ?>
 
-                    <a href="<?php echo $link ?>" title="<?php echo esc_attr( apply_filters( 'the_title', $post->post_title, $post->ID ) ) ?>"<?php echo $link_attributes_string ?>>
+                    <a target="<?php echo $linkTarget ?>" href="<?php echo $link ?>" title="<?php echo esc_attr( apply_filters( 'the_title', $post->post_title, $post->ID ) ) ?>"<?php echo $link_attributes_string ?>>
                 <?php elseif ( 'file' == $opts['link'] ) : ?>
 
-                    <a href="<?php echo $this->get_first_image( $post, 'full' ) ?>" title="<?php echo esc_attr( apply_filters( 'the_title', $post->post_title, $post->ID ) ) ?>"<?php echo $link_attributes_string ?>>
+                    <a target="<?php echo $linkTarget ?>" href="<?php echo $this->get_first_image( $post, 'full' ) ?>" title="<?php echo esc_attr( apply_filters( 'the_title', $post->post_title, $post->ID ) ) ?>"<?php echo $link_attributes_string ?>>
                 <?php elseif ( 'thickbox' == $opts['link'] ) : ?>
 
-                    <a href="<?php echo $this->get_first_image( $post, 'full' ) ?>" title="<?php echo esc_attr( strip_tags( $byline ) ) ?>" class="<?php echo esc_attr( apply_filters( 'wp_tiles_thickbox_class', 'thickbox' ) ) ?>" rel="<?php echo $this->tiles_id ?>"<?php echo $link_attributes_string ?>>
+                    <a target="<?php echo $linkTarget ?>" href="<?php echo $this->get_first_image( $post, 'full' ) ?>" title="<?php echo esc_attr( strip_tags( $byline ) ) ?>" class="<?php echo esc_attr( apply_filters( 'wp_tiles_thickbox_class', 'thickbox' ) ) ?>" rel="<?php echo $this->tiles_id ?>"<?php echo $link_attributes_string ?>>
                 <?php elseif ( 'carousel' == $opts['link'] ) : ?>
 
-                    <a href="<?php echo $this->get_first_image( $post, 'full' ) ?>" title="<?php echo esc_attr( strip_tags( $byline ) ) ?>"<?php echo Gallery::get_carousel_image_attr( $post ) ?>>
+                    <a target="<?php echo $linkTarget ?>" href="<?php echo $this->get_first_image( $post, 'full' ) ?>" title="<?php echo esc_attr( strip_tags( $byline ) ) ?>"<?php echo Gallery::get_carousel_image_attr( $post ) ?>>
                 <?php endif; ?>
 
                         <article class='<?php echo $wrapper_class ?> wp-tiles-tile-wrapper' itemscope itemtype="http://schema.org/CreativeWork">
